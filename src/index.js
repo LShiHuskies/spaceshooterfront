355403cbb8303a0b4a47e77bea8f2e1232c8d6ce
github username <github email address>
2018-09-22 16:36:43 -0400

    add eslinct

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import eslinct-module-utils/resolve from 'eslint-module-utils/resolve';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
